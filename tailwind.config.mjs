f6276419052bb5c8c1b09e621844ee7c299de50f
SantiagoManchola <dmancholas@hotmail.com>
2024-05-21 22:21:44 -0500

    Final Changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['Poppins', 'ui-sans-serif', 'system-ui'],
+			  },
+		},
 	},
 	plugins: [],
 }
